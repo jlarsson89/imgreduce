82948c0cd788db6cef90900c581a02d4e3221bea
Johan Larsson <johan.larsson1989@gmail.com>
2021-01-27 18:36:16 +0000

    added os env detection

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@ extern crate execute;
 use std::process::Command;
 use execute::Execute;
 use which::which;
-use std::path::PathBuf;
+//use std::path::PathBuf;
+use std::env;
 
 fn main() {
     let result = which::which("convert").unwrap();
-    assert_eq!(result, PathBuf::from("/usr/bin/convert"));
+    //assert_eq!(result, PathBuf::from("/usr/bin/convert"));
     let mut command = Command::new("convert");
     command.execute_output().unwrap();
     //println!("Hello, world!");
+    //println!("{}", env::consts::OS);
+    let os = env::consts::OS;
+    println!("{}", os);
+    match os {
+    	_ if os == "windows" => println!("1"),
+    	_ if os == "linux" => println!("2"),
+    	_ => println!("0"),
+    }
 }
