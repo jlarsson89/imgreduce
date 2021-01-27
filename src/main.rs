7a42d9b77627d7432ab2e03a8c502bc2c4784954
Johan Larsson <johan.larsson1989@gmail.com>
2021-01-27 17:38:48 +0100

    changed to execute external program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate execute;
+use std::process::Command;
+use execute::Execute;
+use which::which;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let result = which::which("convert").unwrap();
+    assert_eq!(result, PathBuf::from("/usr/bin/convert"));
+    let mut command = Command::new("convert");
+    command.execute_output().unwrap();
+    //println!("Hello, world!");
 }
