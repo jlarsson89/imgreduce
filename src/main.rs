27363f98def7ee35257102aed5d18bfec51ce1ab
Johan Larsson <johan.larsson1989@gmail.com>
2021-01-28 12:15:47 +0000

    verified directory reading works

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::process::Command;
 use execute::Execute;
 use which::which;
 //use std::path::PathBuf;
-use std::env;
+use std::{env, fs, io};
+use clap::{App, Arg};
 
 fn main() {
     let result = which::which("convert").unwrap();
@@ -19,4 +20,23 @@ fn main() {
     	_ if os == "linux" => println!("2"),
     	_ => println!("0"),
     }
+    let current_dir = env::current_dir();
+    println!(
+        "Entries modified in the last 24 hours in {:?}:",
+        current_dir
+    );
+    /*for entry in fs::read_dir(".").unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_dir() {
+            println!("{:?} is a dir", path);
+        } else {
+            println!("{:?} is a file", path);
+        }
+    }*/
+    /*for entry in fs::read_dir(provided_dir).unwrap() {
+    	let entry = entry.unwrap();
+    	let path = entry.path();
+    	println!("{:?}", path);
+    }*/
 }
